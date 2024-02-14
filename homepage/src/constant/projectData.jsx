a38426b57d0c782a2b162f5cb90343e73e03005d
jiwon0122 <wldnjssem6@naver.com>
2024-02-14 22:00:43 +0900

    feat: main page Project section
    image data dummy

---
+++ homepage/src/constant/projectData.jsx
@@ -0,0 +1,41 @@
+import a from '../assets/images/projectimage/1.JPG'
+import b from '../assets/images/projectimage/2.JPG'
+import c from '../assets/images/projectimage/3.JPG'
+import d from '../assets/images/projectimage/4.JPG'
+import e from '../assets/images/projectimage/5.JPG'
+import f from '../assets/images/projectimage/6.JPG'
+
+
+export const projectData=[
+    {
+        image:a,
+    },
+    {
+        image:b,
+    },
+    {
+        image:c,
+    },
+    {
+        image:d,
+    },
+    {
+        image:e,
+    },
+    {
+        image:f,
+    },
+
+    {
+        image:a,
+    },
+    {
+        image:b,
+    },
+    {
+        image:c,
+    },
+    {
+        image:d,
+    },
+]
