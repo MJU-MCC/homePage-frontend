bfbffc31ad897170b84247ea36559709908fd392
JIWON CHOI <wldnjssem6@naver.com>
2024-03-12 20:26:59 +0900

    feature: login button conditoin add
    login button condition, logout handler add

--- homepage/src/components/Header.jsx
+++ homepage/src/components/Header.jsx
@@ -1,26 +1,50 @@
 import React from 'react';
 import Logo from '../assets/images/Logo_img.png'
 import '../styles/section/_header.scss'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/scss/bootstrap.scss';
 
 const Header = () => {
+    const navigate=useNavigate()
+
+    const loginStatus=()=>{
+        const token=localStorage.getItem("accessToken")
+
+        if(token){
+            return <button type='button' className='btn btn-outline-primary'>Logout</button>
+        }else{
+            return <button type='button' className='btn btn-outline-primary'>Login</button>
+        }
+    }
+
+    const logoutHandler=()=>{
+        localStorage.removeItem("accessToken")
+        navigate('/login')
+        console.log(localStorage.getItem())
+        console.log("logout")
+    }
+
+    const mypageHandler=()=>{
+        const tokencheck=localStorage.getItem('accessToken')
+        if(tokencheck){
+            navigate('/mypage')
+        }else{
+            navigate('/login')
+        }
+    }
     return (
         <header id='header' role='banner'>
             <div className='header__inner'>
-                <div className='header__logo'>
-                    <a href='/'><img src={Logo} alt='Logo'></img></a>
-                </div>
-
+                <a href='/'><img src={Logo} alt='Logo'></img></a>
                 <div className='Nav'>
                 <span>About</span>
                 <span>Study</span>
                 <span>Project</span>
                 <span>Vote</span>
-                <span>MyPage</span>
-                <Link to='/login'>
-                    <button type="button" className="btn btn-outline-primary">Login</button>
+                <span onClick={mypageHandler}>MyPage</span>
+                <Link to='/login' onClick={logoutHandler}>
+                    {loginStatus()}
                 </Link>
                 </div>
             </div>
