81898b92e8739958d95cc293c6c1a92f1a4fe448
JIWON CHOI <wldnjssem6@naver.com>
2024-03-12 20:25:43 +0900

    feature: admin page axios add
    admin page axios add and 'Notion','Instagram','Github' href add

--- homepage/src/components/Footer.jsx
+++ homepage/src/components/Footer.jsx
@@ -1,29 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/section/_footer.scss'
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const Footer = () => {
-    const [isLogin, setIsLogin]=useState(false)
     const navigate=useNavigate();
 
-    useEffect(()=>{
-        const token=localStorage.getItem("accessToken")
+    const adminClick=async()=>{
+        const token=localStorage.getItem('accessToken')
         if(token){
-            setIsLogin(true)
-        }else{
-            setIsLogin(false)
-        }
-    },[])
-
-    const adminClick=()=>{
-        if(isLogin){
-            //운영진 확인 api 
+            try{
+                const info=await axios.get("http://localhost:8080/member/get/myinfo",{
+                    headers:{
+                        Authorization:token.accessToken,
+                    }
+                })
+                if(info.status===200){
+                    console.log(info.data)
+                    if(info.data.user===false){
+                        navigate('/admin')
+                    }else if(info.data.user===true){
+                        alert("관리자만 접속할 수 있습니다.")
+                        navigate('/')
+                    }
+                }
+            }catch(error){
+                if(error.info===404){
+                    navigate('/error')
+                }else{
+                    console.log(error.status)
+                }
+            }
         }else{
             navigate('/login')
         }
     }
-
     return (
         <footer id='footer' role='footer'>
             <div className='footer__inner'>
@@ -31,9 +42,7 @@ const Footer = () => {
                     <span><a href='https://www.notion.so/mjucodingclub/45-95d6016f30cf4449b35ee2f397ecc8ca?pvs=4'>Notion</a></span>
                     <span><a href='https://www.instagram.com/mjumcc/'>Instagram</a></span>
                     <span><a href='https://github.com/MJU-MCC'>Github</a></span>
-                    <Link to='/admin'>
-                        <span>Admin</span>
-                    </Link>
+                    <span onClick={adminClick}>Admin</span>
                 </div>
                 
                 <div className='text'>
