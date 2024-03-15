e585acff4b1a37eacc5a2a714cc635c221438bb7
JIWON CHOI <wldnjssem6@naver.com>
2024-03-15 20:48:55 +0900

    fix: 403 error 해결
    더 수정해야됨

--- homepage/src/components/PwdChange.jsx
+++ homepage/src/components/PwdChange.jsx
@@ -7,48 +7,39 @@ import axios from 'axios';
 const PwdChange = () => {
     const [pwd, setPwd]=useState('');
     const [nextPwd,setNextPwd]=useState('')
+    const [checkNextPwd, setCheckNextPwd]=useState('')
     const navigate=useNavigate();
     
-    const pwdChange=async(e)=>{
-        setNextPwd(e.target.value);
-        try{
-            const info=await axios.post("http://localhost:8080/member/change/password",{
-                nextPassword:nextPwd,
-            })
-            if(info.status===200){
-                console.log("변경되었습니다.")
-            }
-        }catch(error){
-            if(error.nextpassword.status ===404){
-                navigate('/error')
-            }else{
-                console.log(error.status)
-            }
-        }
-    }
 
     const onPwdCheck=async(e)=>{
+        e.preventDefault()
         const token=localStorage.getItem('accessToken')
         try{
             if(token){
-                const res=await axios.get("http://localhost:8080/member/get/myinfo",{
+                const res=await axios.post("http://localhost:8080/member/change/password",{
+                    currentPassword:pwd,
+                    nextPassword:nextPwd,
+                },{
                     headers:{
                         Authorization:token,
                     },
                 })
                 if(res.status===200){
-                    if(res.data.currentPassWord===pwd){ //이부분 api 수정 후 다시 
-                        pwdChange();
-                    }else{
-                        alert("비밀번호가 틀렸습니다.")
+                    if(res.data.currentPassword===pwd){ 
+                        if(nextPwd===checkNextPwd){
+                                alert("비밀번호가 변경되었습니다.")
+                                navigate('/login')
+                        }else{
+                            alert("변경 비밀번호가 일치하지 않습니다.")
+                        }
                     }
                 }
-            }else{
-                navigate('/login')
             }
         }catch(error){
-            if(error.info===404){
+            if(error.response&&error.response.status===404){
                 navigate('/error')
+            }else if(error.response&&error.response.status===400){
+                alert(error.response.data.message)
             }else{
                 console.log(error.status)
             }
@@ -56,16 +47,19 @@ const PwdChange = () => {
     }
 
     const handleSubmit=(e)=>{
-        onPwdCheck()
+        e.preventDefault()
+        onPwdCheck(e)
     }
     return (
         <section id='change'>
             <div className='inner__change'>
                 <form onSubmit={handleSubmit}>
-                    <input type='text' value={pwd} onChange={(e)=>setPwd(e.target.value)} placeholder='현재 비밀번호' class="p-3 bg-info bg-opacity-10 border border-info border-start rounded"></input><br/>
-                    <input type='text' value={nextPwd} onChange={(e)=>setNextPwd(e.target.value)} placeholder='변경할 비밀번호' class="p-3 bg-info bg-opacity-10 border border-info border-start rounded"></input><br/>
+                    <input type='password' value={pwd} onChange={(e)=>setPwd(e.target.value)} placeholder='현재 비밀번호' class="p-3 bg-info bg-opacity-10 border border-info border-start rounded"></input><br/>
+                    <input type='password' value={nextPwd} onChange={(e)=>setNextPwd(e.target.value)} placeholder='변경할 비밀번호' class="p-3 bg-info bg-opacity-10 border border-info border-start rounded"></input><br/>
+                    <input type='password' value={checkNextPwd} onChange={(e)=>setCheckNextPwd(e.target.value)} placeholder='변경할 비밀번호 확인' class="p-3 bg-info bg-opacity-10 border border-info border-start rounded"></input><br/>
                     <button type='submit' className='btn btn-outline-primary'>비밀번호 변경</button>
                 </form>
+                
             </div>
             
         </section>
